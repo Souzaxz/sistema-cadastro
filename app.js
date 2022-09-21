a85bb22515afe279a824155ef17957b0a17e160a
GabrielCabide <110506680+GabrielCabide@users.noreply.github.com>
2022-09-21 20:39:18 -0300

    Atualizado App.js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-console.log("Rodando sistema de gernciamento de jogos")
+console.log("Rodando sistema de gernciamento de jogos");
+console.log("Sonim blenem sinim, sinim blenem sonim clever, sonim clever sinim blenem");
